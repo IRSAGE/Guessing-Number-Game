a1d12f40775b1766c38d9343c90ab50ac7c64e80
IR~SAGE <sibo.egide98@gmail.com>
2021-07-18 12:01:14 +0200

    Adding A StartGameScreen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Header } from "react-native/Libraries/NewAppScreen";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
+      <StartGameScreen />
     </View>
   );
 }
