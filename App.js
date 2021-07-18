4953ad91350edf08f31fd9d958f3e3c819095f19
IR~SAGE <sibo.egide98@gmail.com>
2021-07-18 11:21:05 +0200

    Designing The Header

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
-    <View>
-      
+    <View style={styles.screen}>
+      <Header title="Guess A Number" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- 
+  screen: {
+    flex: 1,
+  },
 });
