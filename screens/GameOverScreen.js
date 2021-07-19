7fbef1777d0a2e78c0e60ba7b29e6d22a6007e4d
IR~SAGE <sibo.egide98@gmail.com>
2021-07-19 08:22:43 +0200

    Working With Images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 const GameOverScreen = () => {
   return (
     <View style={styles.screen}>
       <Text>The Game Is Over</Text>
+      <View style={styles.imageContainer}>
+        <Image source={require("../assets/success.png")} style={styles.image} />
+      </View>
       <Text>Nunber Of Rounds: {props.roundsNumber}</Text>
       <Text>Nunber Was: {props.userNumber}</Text>
       <Button title="New Game" onPress={props.onRestart} />
@@ -17,6 +20,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
