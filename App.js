3bac88b927c52466cf483a0085f21a47150db2de
IR~SAGE <sibo.egide98@gmail.com>
2021-07-19 07:24:18 +0200

    Finishing The Game Logic

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGessRounds] = useState(0);
 
+  const configureNewGameHandler = () => {
+    setGessRounds(0);
+    setUserNumber(Null);
+  };
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
     setGessRounds(0);
@@ -24,7 +29,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
   return (
     <View style={styles.screen}>
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { View, Text ,StyleSheet} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = () => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Game Is Over</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.screen}>
+      <Text>The Game Is Over</Text>
+      <Text>Nunber Of Rounds: {props.roundsNumber}</Text>
+      <Text>Nunber Was: {props.userNumber}</Text>
+      <Button title="New Game" onPress={props.onRestart} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems:'center',
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default GameOverScreen
+export default GameOverScreen;
